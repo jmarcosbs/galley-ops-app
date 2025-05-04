4b18d633160ecd67c3727b905341218ecc69c102
João Marcos <jmarcosmmn@gmail.com>
2025-05-03 23:20:18 -0300

    refactor: streamline HandleOrderItem component by using dishUniqueId and cleaning up JSX structure

--- src/app/components/HandleOrderItem.tsx
+++ src/app/components/HandleOrderItem.tsx
@@ -1,22 +1,19 @@
+import { useOrderContext } from '../context/OrderContext';
 import CommentOrder from './CommentOrder';
-import DeleteOrder from './DeleteOrder'
+import DeleteOrder from './DeleteOrder';
 
 interface HandleOrderItemProps {
-    dishIndex : number;
+    dishIndex: number;
 }
 
-export default function HandleOrderItem({ dishIndex } : HandleOrderItemProps) {
-
+export default function HandleOrderItem({ dishIndex }: HandleOrderItemProps) {
+    const { dishes } = useOrderContext();
+    const dishUniqueId = dishes[dishIndex].unique_id;
 
     return (
-        <>
-            <div className='flex items-center flex-col'>
-
-                <CommentOrder dishIndex={dishIndex}/>
-
-                <DeleteOrder dishIndex={dishIndex}/>
-
-            </div>
-        </>
+        <div className='flex items-center flex-col'>
+            <CommentOrder dishUniqueId={dishUniqueId} />
+            <DeleteOrder dishIndex={dishIndex} />
+        </div>
     );
 }
